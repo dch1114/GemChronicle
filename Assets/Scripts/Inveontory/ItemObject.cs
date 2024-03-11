f63986e395647f5110b3ddeccdfcfbdef76069a6
shss1105 <kimseung2331@naver.com>
2024-03-11 11:09:22 +0900

    [feat]240311_에셋 추가 등
    에셋 - 아이템 테스트 하기 위한 무료 에셋 추가
    
    인벤토리 - 아이템 위에서 z키 누르면 획득
                - 장비 아이템 누르면 장비 칸으로 아이템 이동

---
+++ Assets/Scripts/Inveontory/ItemObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    [SerializeField] public ItemData itemData;  // public ¹Ù²Ü¹æ¹ý
+
+    private void OnValidate()
+    {
+        GetComponent<SpriteRenderer>().sprite = itemData.icon;
+        gameObject.name = "item object - " + itemData.name;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.GetComponent<Player>() != null)
+    //    {
+    //        Inventory.Instance.AddItem(itemData);
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Scripts/Inveontory/UI_ItemSlot.cs
@@ -0,0 +1,51 @@
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TextMeshProUGUI itemText;
+
+    public InventoryItem item;
+
+    public void UpdateSlot(InventoryItem _newitem)
+    {
+        item = _newitem;
+
+        itemImage.color = Color.white;
+
+        if(item != null)
+        {
+            itemImage.sprite = item.data.icon;
+
+            if (item.stackSize > 1)
+            {
+                itemText.text = item.stackSize.ToString();
+            }
+            else
+            {
+                itemText.text = "";
+            }
+        }
+    }
+
+    public void CleanUpSlot()
+    {
+        item = null;
+
+        itemImage.sprite = null;
+        itemImage.color = Color.clear;
+        itemText.text = "";
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (item.data.itemType == ItemType.Equipment)
+        {
+            Inventory.Instance.EquipItem(item.data);
+        }
+
+    }
+}
