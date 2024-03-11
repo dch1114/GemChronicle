25832f038c2c4d71162691d94a61010f86a76c61
shss1105 <kimseung2331@naver.com>
2024-03-11 17:37:43 +0900

    [fix]240311_비어있는 장비 아이템 슬롯 클릭 시 발생하는 에러 수정

--- Assets/Scripts/Inveontory/UI_EquipmentSlot.cs
+++ Assets/Scripts/Inveontory/UI_EquipmentSlot.cs
@@ -14,6 +14,11 @@ public class UI_EquipmentSlot : UI_ItemSlot
 
     public override void OnPointerDown(PointerEventData eventData)
     {
+        if(item == null || item.data == null)
+        {
+            return;
+        }
+
         Inventory.Instance.UnEquipItem(item.data as ItemData_Equipment);
         Inventory.Instance.AddItem(item.data as ItemData_Equipment);
         CleanUpSlot();
