a15a477568455361ffcea4dc7be7bb8d60796638
purangi <kaya0728@naver.com>
2024-03-12 17:37:10 +0900

    Feat: isGrounded RayCast로 변경

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -6,12 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public Vector2 velocity;
-    public bool isGrounded { get; } = true;
+    public bool isGrounded { get; set; } = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         velocity = rb.velocity;
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 0.1f, LayerMask.GetMask("Ground"));
+        Debug.DrawRay(transform.position, Vector3.down, Color.green);
+        if(hit.collider != null)
+        {
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
     }
 
     public void Move(Vector3 _speed)
