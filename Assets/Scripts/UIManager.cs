d755b79f25d301dbf49ff27a9296654d4e0bf40d
dch1114 <dch1114@naver.com>
2024-03-17 20:11:37 +0900

    [feat]240317_UIManager 수정

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,22 +12,21 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject panelInspectorQuests;
     [SerializeField] private GameObject panelPersonajeQuests;
     public enum ShowMenuType  //보기쉽게 하기위해 enum선언
-    { 
+    {
         Buy,
         Sell,
         Exit,
         Max
     }
-    
+
 
     public GameObject talkBtn;
     public GameObject potraitPanel;
     public GameObject shopPanel;
     public GameObject shopChoice;
-    public GameObject btn;
     public static UIManager instance = null;
 
-    public static UIManager instance = null;
+
 
     public PlayerController playerController;
 
@@ -35,18 +34,24 @@ public class UIManager : MonoBehaviour
 
     public Sprite selectButton;
     public Sprite unSelectButton;
-    [SerializeField]    
+    [SerializeField]
     ShowMenuType currentShowMenuType;
 
     bool isOpenShowPopUp = false;
     public PlayerInput playerinput;
     Action selectMenuAction = null;
 
+    //0315 [SerializeField]를 선언하면 외부 스크립트에서 접근할수 없으나 인스펙터에서 세팅 및 확인을 할 수 있음  
+    [SerializeField]
+    Text talkText;
+    [SerializeField]
+    Image portraitImg;
+
     void Awake()
     {
         if (instance == null)
         {
-            instance = this; 
+            instance = this;
         }
         else
         {
@@ -75,9 +80,16 @@ public class UIManager : MonoBehaviour
         showMenuButton[(int)ShowMenuType.Exit].image.sprite = unSelectButton;
     }
 
-
-    
-
+    //0315 대회메세지 세팅
+    public void SetTalkMessage(string msg)
+    {
+        talkText.text = msg;
+    }
+    //0315 초상화 이미지 세팅
+    public void SetPortraitImage(Sprite sp)
+    {
+        portraitImg.sprite = sp;
+    }
     public void talkBtnOnOff(bool _OnOff)
     {
         talkBtn.SetActive(_OnOff);
@@ -95,6 +107,7 @@ public class UIManager : MonoBehaviour
 
     public void shopChoiceOnOff(bool _OnOff)
     {
+
         if (_OnOff)
         {
             Cursor.lockState = CursorLockMode.None;
@@ -108,13 +121,91 @@ public class UIManager : MonoBehaviour
         }
         else
         {
-            NPCInteractive.instance.isAction = false;
-            talkPanelOnOff(false);
-            btn.SetActive(_OnOff);
+
+            //NPCInteractive.instance.isAction = false;
+            PotraitPanelOnOff(false);
+            //btn.SetActive(_OnOff);
             shopChoice.SetActive(_OnOff);
+            playerinput.OnEnable();
+            isOpenShowPopUp = false;
+
         }
+
+
+    }
+    public void PopupShopMenuSelect(ShowMenuType type)
+    {
+        if (currentShowMenuType > ShowMenuType.Exit)
+        {
+            currentShowMenuType = ShowMenuType.Exit;
+
+            return;
+        }
+
+        if (currentShowMenuType < ShowMenuType.Buy)
+        {
+            currentShowMenuType = ShowMenuType.Buy;
+
+            return;
+        }
+
+
+        currentShowMenuType = type;
+        //기존에 세팅되어 있는 버튼의 연결을 모두 해제
+        selectMenuAction = null;
+        //선택될 버튼에 상점타입의 버튼을 연결한다 
+        selectMenuAction = GetSelectedShopMenu(type);
+
+    }
+    public void RunSelectedMenuButton()
+    {
+        selectMenuAction?.Invoke();
     }
 
+    Action GetSelectedShopMenu(ShowMenuType type)
+    {
+        showMenuButton[(int)ShowMenuType.Buy].image.sprite = unSelectButton;
+        showMenuButton[(int)ShowMenuType.Sell].image.sprite = unSelectButton;
+        showMenuButton[(int)ShowMenuType.Exit].image.sprite = unSelectButton;
+
+        switch (type)
+        {
+            case ShowMenuType.Buy:
+                showMenuButton[(int)ShowMenuType.Buy].image.sprite = selectButton;
+
+                return BuyShop;
+
+            case ShowMenuType.Sell:
+                showMenuButton[(int)ShowMenuType.Sell].image.sprite = selectButton;
+
+                return SellShop;
+            case ShowMenuType.Exit:
+                showMenuButton[(int)ShowMenuType.Exit].image.sprite = selectButton;
+
+                return ExitShop;
+            default:
+                return null;
+        }
+    }
+    void BuyShop()
+    {
+
+        Debug.Log("Select Buy");
+    }
+
+    void SellShop()
+    {
+
+        Debug.Log("Select Sell");
+
+    }
+
+    void ExitShop()
+    {
+        shopChoiceOnOff(false);
+        Debug.Log("Select Exi");
+
+    }
     public void OpenClosePanelInspectorQuests()
     {
         panelInspectorQuests.SetActive(!panelInspectorQuests.activeSelf);
@@ -123,5 +214,20 @@ public class UIManager : MonoBehaviour
     public void AbrirCerrarPanelPersonajeQuests()
     {
         panelPersonajeQuests.SetActive(!panelPersonajeQuests.activeSelf);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            PopupShopMenuSelect(--currentShowMenuType);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            //Debug.Log("Key Down");
+            PopupShopMenuSelect(++currentShowMenuType);
+            //Debug.Log("현재 선택된 메뉴는 : " + currentShowMenuType);
+        }
+
     }
 }
