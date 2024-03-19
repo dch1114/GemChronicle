88e45effc128db97167a9b5c93fbb4736f56bea5
purangi <kaya0728@naver.com>
2024-03-19 14:14:54 +0900

    Feat: Enemy 상태 및 애니메이션 데이터 추가

--- Assets/Scripts/Characters/Enemy/Enemy.cs
+++ Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyState
+{
+    Idle,
+    Walking,
+    Attacking,
+    Dead
+}
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] public EnemyStatusData EnemyStatusData;
 
-
+    protected static readonly int 
 }
