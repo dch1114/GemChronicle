28552f90d3621b70c60696de94b6b9c4bfce2f50
purangi <kaya0728@naver.com>
2024-03-19 14:32:59 +0900

    Fix: Enemy Move 스크립트 없애고 Enemy로 옮김

--- Assets/Scripts/Characters/Enemy/Enemy.cs
+++ Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -13,6 +13,62 @@ public enum EnemyState
 public class Enemy : MonoBehaviour
 {
     [SerializeField] public EnemyStatusData EnemyStatusData;
+    [SerializeField] public EnemyAnimationData EnemyAnimationData;
 
-    protected static readonly int 
+    private Rigidbody2D rigid;
+    private Animator animator;
+
+    private int nextMove;
+    private bool isLeft = true;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        Invoke("Think", 5);
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        if (nextMove != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+
+        if ((nextMove > 0 && isLeft) || (nextMove < 0 && !isLeft))
+        {
+            Flip();
+        }
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.4f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
+        if (rayHit.collider == null)
+        {
+            nextMove = nextMove * -1;
+            CancelInvoke();
+            Invoke("Think", 2);
+        }
+    }
+
+    // Àç±Í ÇÔ¼ö
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+        animator.SetBool("isMoving", false);
+
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+
+        transform.localScale = scale;
+        isLeft = !isLeft;
+    }
 }
