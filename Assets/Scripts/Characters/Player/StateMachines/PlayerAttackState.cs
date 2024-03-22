6041c02afb27e6047eba4403f20340fc6263c9d0
purangi <kaya0728@naver.com>
2024-03-22 19:01:50 +0900

    Feat: 스킬 프리팹 생성 및 출력

--- Assets/Scripts/Characters/Player/StateMachines/PlayerAttackState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/PlayerAttackState.cs
@@ -10,7 +10,7 @@ public class PlayerAttackState : PlayerBaseState
 
     public override void Enter()
     {
-        stateMachine.MovementSpeedModifier = 0;
+        //stateMachine.MovementSpeedModifier = 0f;
         base.Enter();
 
         StartAnimation(stateMachine.Player.AnimationData.AttackParameterHash);
--- Assets/Scripts/Characters/Player/StateMachines/PlayerJumpState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/PlayerJumpState.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerJumpState : PlayerAirState
 {
-    private bool isJumping = false;
     public PlayerJumpState(PlayerStateMachine _playerStateMachine) : base(_playerStateMachine)
     {
     }
