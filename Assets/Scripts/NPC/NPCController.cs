f974d6175bf95cab0a587165ce932b29f662eba7
LeeGunHyung <154494501+Woodong96@users.noreply.github.com>
2024-03-28 14:47:10 +0900

    Fix Json 버그수정

--- Assets/Scripts/NPC/NPCController.cs
+++ Assets/Scripts/NPC/NPCController.cs
@@ -148,133 +148,176 @@ public class NPCController : MonoBehaviour, IInteractive
         //초상화 이미지 ON
         uiManager.PotraitPanelOnOff(true);
 
-
-        //더이상 대화 내용이 존재 하지 않아 대화를 종료해야 한다면
-        if (CheckConversationCount())
-        {
-            //초상화 OFF
-            uiManager.PotraitPanelOnOff(false);
-
-            if (npcType == NPCType.Shop)
-            {
-                //플레이어 이동 불가
-                playerinput.OnDisable();
-                //상점 팝업창 ON
-                uiManager.shopChoiceOnOff(true);
-            }
-
-            return;
-        }
-
         Talk();
 
     }
 
-
-
     void Talk()
     {
-        ////대화 불러오기
-        //int msgId = GetNpcData().ID;
-        //string talkData = talkManager.GetTalk(msgId, talkIndex);
-
-        //if (talkData == null)
-        //{
-
-        //    talkIndex = 0;
-        //    isEndTalk = true;
-        //    uiManager.SetTalkMessage(talkData);
-        //    uiManager.PotraitPanelOnOff(false);
-        //    return;
-        //}
-        //else
-        //{
-
-        //    //isAction = true;
-        //    uiManager.PotraitPanelOnOff(true);
-        //    uiManager.SetTalkMessage(talkData);
-        //    uiManager.SetNpcPortraitImage(talkManager.GetPortrait(msgId));
-        //    talkIndex++;
-
-        //}
-        //uiManager.SetTalkMessage(talkData);
-        //talkIndex++;
-
-
-
-        if (sayingNPC && talkIndex + 1 < npcMsgList.Count)
+        while (true)
         {
-
-            if (npcMsgList[talkIndex].Length > 0)
+            //NPC와 PLAYER 둘다 더이상 할 대화가 남아있지 않아 대화를 종료해야 한다면
+            if (talkIndex >= npcMsgList.Count && talkIndex >= playerMsgList.Count)
             {
-                //Npc가 말하는 중이고 아직 더 할말이 있다면
-                uiManager.SetTalkMessage(npcMsgList[talkIndex][talkNpcStep]);
-                uiManager.ShowNpcPotrait(true);
-                uiManager.ShowPlayerPotrait(false);
-                talkNpcStep++;
-
-                //대화 문장이 끝이 났다면
-                if (talkNpcStep >= npcMsgList[talkIndex].Length)
-                {
-                    //Player가 말할 차례 설정
-                    sayingNPC = false;
-                    //Player가 이미 할말을 다했다면
-                    if (talkPlayerStep >= playerMsgList[talkIndex].Length)
-                    {
-                        //다음 대화 문장으로
-                        talkIndex++;
-                    }
-                }
-            }
-            else
-            {
+                isEndTalk = true;
+                playerinput.OnEnable();
+                talkIndex = 0;
+                talkNpcStep = 0;
                 talkPlayerStep = 0;
 
-                sayingNPC = false;
+                //초상화 OFF
+                uiManager.PotraitPanelOnOff(false);
 
-                Talk();
+                if (npcType == NPCType.Shop)
+                {
+                    //플레이어 이동 불가
+                    playerinput.OnDisable();
+                    //상점 팝업창 ON
+                    uiManager.shopChoiceOnOff(true);
+                }
+
+                return;
             }
 
+            // 현재 대화 주체가 NPC인지 판별
+            bool isNPCSpeaking = (sayingNPC && talkIndex < npcMsgList.Count && talkNpcStep < npcMsgList[talkIndex].Length);
 
-        }
-        //Player가 말 할 차례이고 문장이 더 존재한다면
-        else if (!sayingNPC && talkIndex + 1 < playerMsgList.Count)
-        {
+            // 플레이어 대화 메시지가 비어있는지 확인
+            bool playerHasMessages = talkIndex < playerMsgList.Count && playerMsgList[talkIndex].Length > 0;
 
-            if (playerMsgList[talkIndex].Length > 0)
+            // 대화 메시지 선택
+            string[] currentMessages;
+            int currentStep;
+
+            //NPC가 말할때
+            if (isNPCSpeaking)
             {
-
-                uiManager.SetTalkMessage(playerMsgList[talkIndex][talkPlayerStep]);
-                uiManager.ShowNpcPotrait(false);
-                uiManager.ShowPlayerPotrait(true);
-
-                talkPlayerStep++;
-
-                if (talkPlayerStep >= playerMsgList[talkIndex].Length)
-                {
-                    //Npc가 말할 차례 설정
-                    sayingNPC = true;
-                    //Npc가 이미 할말을 다했다면
-                    if (talkNpcStep >= npcMsgList[talkIndex].Length)
-                    {
-                        //다음 대화 문장으로
-                        talkIndex++;
-                    }
-
-                }
+                currentMessages = npcMsgList[talkIndex];
+                currentStep = talkNpcStep;
+            }
+            else if (playerHasMessages)//플레이어가 말할 내용이 있을때
+            {
+                currentMessages = playerMsgList[talkIndex];
+                currentStep = talkPlayerStep;
             }
             else
             {
-                talkNpcStep = 0;
-
-                sayingNPC = false;
-
-                Talk();
+                // 플레이어가 말할 내용이 없을 때는 다음 대화로 이동
+                talkIndex++;
+                sayingNPC = true; // 다음 대화는 NPC가 시작
+                continue;
             }
 
+            // 대화 메시지 출력
+            if (currentStep < currentMessages.Length)
+            {
+                uiManager.SetTalkMessage(currentMessages[currentStep]);
+                uiManager.ShowNpcPotrait(isNPCSpeaking);
+                uiManager.ShowPlayerPotrait(!isNPCSpeaking);
+
+                // 대화 스텝 증가
+                if (isNPCSpeaking)
+                    talkNpcStep++;
+                else
+                    talkPlayerStep++;
+            }
+
+            // 대화가 끝났으면 다음 대화로 이동
+            if (currentStep >= currentMessages.Length)
+            {
+                // 다음 대화 인덱스로 이동
+                talkIndex++;
+
+                // 다음 대화 주체 설정
+                sayingNPC = !sayingNPC;
+
+                // 대화 스텝 초기화
+                if (isNPCSpeaking)
+                    talkNpcStep = 0;
+                else
+                    talkPlayerStep = 0;
+
+                // 다음 대화로 이동
+                continue;
+            }
+
+            break;
 
         }
 
+        //if (sayingNPC && talkIndex + 1 < npcMsgList.Count)
+        //{
+
+        //    if (npcMsgList[talkIndex].Length > 0)
+        //    {
+        //        //Npc가 말하는 중이고 아직 더 할말이 있다면
+        //        uiManager.SetTalkMessage(npcMsgList[talkIndex][talkNpcStep]);
+        //        uiManager.ShowNpcPotrait(true);
+        //        uiManager.ShowPlayerPotrait(false);
+        //        talkNpcStep++;
+
+        //        //대화 문장이 끝이 났다면
+        //        if (talkNpcStep >= npcMsgList[talkIndex].Length)
+        //        {
+        //            //Player가 말할 차례 설정
+        //            sayingNPC = false;
+        //            //Player가 이미 할말을 다했다면
+        //            if (talkPlayerStep >= playerMsgList[talkIndex].Length || playerMsgList[talkIndex].Length <= 0)
+        //            {
+        //                //다음 대화 문장으로
+        //                talkIndex++;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        talkPlayerStep = 0;
+
+        //        sayingNPC = false;
+
+        //        Talk();
+        //    }
+
+
+        //}
+        ////Player가 말 할 차례이고 문장이 더 존재한다면
+        //else if (!sayingNPC && talkIndex + 1 < playerMsgList.Count)
+        //{
+
+        //    if (playerMsgList[talkIndex].Length > 0)
+        //    {
+
+        //        uiManager.SetTalkMessage(playerMsgList[talkIndex][talkPlayerStep]);
+        //        uiManager.ShowNpcPotrait(false);
+        //        uiManager.ShowPlayerPotrait(true);
+
+        //        talkPlayerStep++;
+
+        //        if (talkPlayerStep >= playerMsgList[talkIndex].Length)
+        //        {
+        //            //Npc가 말할 차례 설정
+        //            sayingNPC = true;
+        //            //Npc가 이미 할말을 다했다면
+        //            if (talkNpcStep >= npcMsgList[talkIndex].Length || npcMsgList[talkIndex].Length <= 0)
+        //            {
+        //                //다음 대화 문장으로
+        //                talkIndex++;
+        //            }
+
+        //        }
+        //    }
+        //    else
+        //    {
+        //        talkNpcStep = 0;
+
+        //        sayingNPC = false;
+
+        //        Talk();
+        //    }
+
+
+        //}
+        Debug.Log("talkIndex:" + talkIndex);
 
 
     }
@@ -283,7 +326,7 @@ public class NPCController : MonoBehaviour, IInteractive
     bool CheckConversationCount()
     {
         //NPC와 PLAYER 둘다 더이상 할 대화가 남아있지 않다면
-        if (talkIndex + 1 >= npcMsgList.Count && talkIndex + 1 >= playerMsgList.Count)
+        if (talkIndex >= npcMsgList.Count && talkIndex >= playerMsgList.Count)
         {
             isEndTalk = true;
             playerinput.OnEnable();
