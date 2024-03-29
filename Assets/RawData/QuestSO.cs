f4a1a44486de081be0c8156f3a479d4adb267dc7
dch1114 <dch1114@naver.com>
2024-03-29 15:20:07 +0900

    [fix]240329_스크립트 이름 수정

--- Assets/RawData/QuestSO.cs
+++ Assets/RawData/QuestSO.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExcelAsset(AssetPath = "Resources/DB", ExcelName = "QuestDataSheet")]
-public class QuestSO : ScriptableObject
+public class QuestDataSheet : ScriptableObject
 {
 	public List<QuestData> Sheet1; // Replace 'EntityType' to an actual type that is serializable.
 }
