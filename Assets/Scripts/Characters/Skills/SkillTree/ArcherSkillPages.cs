63758ca76db06b0db4163a7c4c76a5d0edf064a5
purangi <kaya0728@naver.com>
2024-04-05 17:54:11 +0900

    Fix: 궁수는 스킬페이지 열자마자 스킬 정보 보이게

--- Assets/Scripts/Characters/Skills/SkillTree/ArcherSkillPages.cs
+++ Assets/Scripts/Characters/Skills/SkillTree/ArcherSkillPages.cs
@@ -10,11 +10,17 @@ public class ArcherSkillPages : SkillPages
 {
     [SerializeField] private Image typeIcon;
     [SerializeField] private TextMeshProUGUI rangeTxt;
+    [SerializeField] private TextMeshProUGUI priceTxt;
     [SerializeField] private GameObject descriptionTxt;
 
     [SerializeField] private List<SkillTreeButton> skillTreeBtns;
 
     private SkillTreeButton currentTreeBtn;
+
+    private void OnEnable()
+    {
+        InitializeSkillInfo();
+    }
 
     protected override void ShowSkillSettings()
     {
@@ -25,6 +31,8 @@ public class ArcherSkillPages : SkillPages
         {
             btn.SetSkillBtn();
         }
+
+        InitializeSkillInfo();
     }
 
     public override void UnlockSkillBtn()
@@ -52,30 +60,38 @@ public class ArcherSkillPages : SkillPages
         ShowSkillSettings();
     }
 
-    public void ShowSkillInfo(SkillTreeButton _skill)
+    public void InitializeSkillInfo()
     {
-        currentTreeBtn = _skill;
-
         int asdIndex = GetASDIndex();
         int index = player.Data.AttackData.AttackSkillStates[asdIndex][0];
+
+        skillIcon.sprite = icons[asdIndex];
 
         SkillInfoData data = player.Data.AttackData.GetSkillInfo(index);
 
         if (descriptionTxt.activeSelf) descriptionTxt.SetActive(false);
 
-        string damageStr = data.Damage.ToString();
-        string rangeStr = data.Range.ToString();
+        damageTxt.text = data.Damage.ToString();
+        rangeTxt.text = data.Range.ToString();
+
         ShowSkillType();
+    }
+
+    public void ShowSkillInfo(SkillTreeButton _skill)
+    {
+        currentTreeBtn = _skill;
+
+        InitializeSkillInfo();
 
         if(!_skill.isUnlocked)
         {
             switch (_skill.GetSkillType())
             {
                 case SkillTreeButton.SkillTreeType.increaseAtk:
-                    damageStr += "<color=green> ( + " + _skill.amount + " )</color>";
+                    damageTxt.text += "<color=green> ( + " + _skill.amount + " )</color>";
                     break;
                 case SkillTreeButton.SkillTreeType.increaseRange:
-                    rangeStr += "<color=green> ( + " + _skill.amount + " )</color>";
+                    rangeTxt.text += "<color=green> ( + " + _skill.amount + " )</color>";
                     break;
                 case SkillTreeButton.SkillTreeType.increaseAtkSphere:
                     descriptionTxt.SetActive(true);
@@ -84,8 +100,7 @@ public class ArcherSkillPages : SkillPages
         }
 
         buyBtn.SetActive(!_skill.isUnlocked);
-        damageTxt.text = damageStr;
-        rangeTxt.text = rangeStr;
+        priceTxt.text = _skill.price > 0 ? _skill.price.ToString() : string.Empty;
     }
 
     private void ShowSkillType()
