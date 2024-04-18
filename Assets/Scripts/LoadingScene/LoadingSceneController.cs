4d737627910b26c01bb53d76251802428cd27b38
shss1105 <kimseung2331@naver.com>
2024-04-18 19:40:43 +0900

    Reapply "Feat : 로딩 씬 게임 팁 추가"
    This reverts commit e8257927eecc6f81af604008292d9dd0421ac7e6.

--- Assets/Scripts/LoadingScene/LoadingSceneController.cs
+++ Assets/Scripts/LoadingScene/LoadingSceneController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ public class LoadingSceneController : MonoBehaviour
     static string nextScene;
 
     [SerializeField] Image progressBar;
+    [SerializeField] TextMeshProUGUI tipText;
 
     public static void LoadScene(string sceneName)
     {
@@ -25,6 +27,7 @@ public class LoadingSceneController : MonoBehaviour
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(nextScene);
         asyncOperation.allowSceneActivation = false;
+        ShowRandomTip();
 
         float timer = 0f;
         while (!asyncOperation.isDone)
@@ -47,5 +50,19 @@ public class LoadingSceneController : MonoBehaviour
             }
         }
 
+    }
+
+    private void ShowRandomTip()
+    {
+        // 여기서 게임 팁 데이터를 가져와서 랜덤하게 하나를 선택하여 표시합니다.
+        string[] tips = {
+            "Tip 1: 게임 팁 1번째.",
+            "Tip 2: 게임 팁 2번째.",
+            "Tip 3: 게임 팁 3번째.",
+            // Add more tips as needed
+        };
+
+        int randomIndex = Random.Range(0, tips.Length);
+        tipText.text = tips[randomIndex];
     }
 }
