a8efc5a524ea4dc819166924453d1fec546012db
shss1105 <kimseung2331@naver.com>
2024-04-23 15:33:28 +0900

    Feat : 엔딩씬 수정

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -1,13 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ending : MonoBehaviour
 {
     public GameObject go;
 
+    public void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Ending")
+        {
+            EndingCredit();
+        }
+    }
+
     public void EndingCredit()
     {
         go.SetActive(true);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
