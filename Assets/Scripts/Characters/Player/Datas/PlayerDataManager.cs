6e0687656c566bc0f8539eae703e3733df0a4f08
purangi <kaya0728@naver.com>
2024-04-23 21:37:40 +0900

    Fix: 일기 대사 수정

--- Assets/Scripts/Characters/Player/Datas/PlayerDataManager.cs
+++ Assets/Scripts/Characters/Player/Datas/PlayerDataManager.cs
@@ -252,7 +252,7 @@ public class PlayerDataManager : Singleton<PlayerDataManager>
 
         if(UIManager.Instance != null)
         {
-            UIManager.Instance.alertPanelUI.ShowAlert("����Ǿ����ϴ�.");
+            UIManager.Instance.alertPanelUI.ShowAlert("저장되었습니다.");
         }
     }
 
